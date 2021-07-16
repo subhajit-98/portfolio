b0bfc1257275c1fce5e946000a3a254825b27bfd
subhajit-98 <subhajitdey.1998@outlook.com>
2021-07-16 13:15:31 +0530

    Education dynamic done

--- src/Component/Education/Education.jsx
+++ src/Component/Education/Education.jsx
@@ -1,6 +1,7 @@
+import React, { useState, useEffect } from 'react';
 import PageTitle from '../../Container/PageTitle/PageTitle'
 import { Container, Grid, Paper, Typography , Box, Button } from '@material-ui/core';
-import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot, TimelineOppositeContent } from '@material-ui/lab';
+import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot, TimelineOppositeContent, Skeleton  } from '@material-ui/lab';
 import FaceRoundedIcon from '@material-ui/icons/FaceRounded';
 import LocationOnRoundedIcon from '@material-ui/icons/LocationOnRounded';
 import ChildFriendlyRoundedIcon from '@material-ui/icons/ChildFriendlyRounded';
@@ -9,9 +10,28 @@ import {NavLink} from "react-router-dom";
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import ArrowRightAltRoundedIcon from '@material-ui/icons/ArrowRightAltRounded';
 import { Helmet } from 'react-helmet';
+import axios from 'axios';
 import './Education.css';
 
 const Education = () => {
+    const [loading, setLoading] = useState(true)
+    const [education, setEducation] = useState();
+    useEffect(() => {
+        if(loading){
+            axios.get(process.env.REACT_APP_API_URL+'education/')
+            .then(res => {
+                // console.log(res);
+                if(res.data.status == 'success'){
+                    setEducation(res.data);
+                    setLoading(false);
+                }
+            })
+            .catch(res => {
+                console.log(res);
+            })
+        }
+    })
+    console.log(education, loading);
     return (
         <Container maxWidth="md">
             <Helmet>
@@ -19,69 +39,70 @@ const Education = () => {
             </Helmet>
             <PageTitle title="Qulifications" />
             <Grid container spacing={2}>
-                <Grid xs={12}>
-                    <Timeline align="alternate">
-                        <TimelineItem>
-                            <TimelineOppositeContent>
-                                <Typography variant="body2" color="text.primar">2021</Typography>
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                <TimelineDot variant="outlined" />
-                                <TimelineConnector className="lineHeight" />
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Paper elevation={3} className="education_background">
-                                    <Typography variant="h6" component="h1">Master's of Computer Applications (MCA)</Typography>
-                                    <Typography>Future Institute of Engneering and Management</Typography>
-                                </Paper>
-                            </TimelineContent>
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineOppositeContent>
-                                <Typography variant="body2" color="secondary">2018</Typography>
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                <TimelineDot  variant="outlined" color="secondary" />
-                                <TimelineConnector className="lineHeight" />
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Paper elevation={3} className="education_background">
-                                    <Typography variant="h6" component="h1">Bachlor's of Computer Applications (BCA)</Typography>
-                                    <Typography>Future Institute of Engneering and Management</Typography>
-                                </Paper>
-                            </TimelineContent>
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineOppositeContent>
-                            <Typography variant="body2" color="primary">2015</Typography>
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                <TimelineDot  variant="outlined" color="primary" />
-                                <TimelineConnector className="lineHeight" />
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Paper elevation={3} className="education_background">
-                                    <Typography variant="h6" component="h1">12<sup>th</sup> Stands</Typography>
-                                    <Typography>Khanput H.L. High School</Typography>
-                                </Paper>
-                            </TimelineContent>
-                        </TimelineItem>
-                        <TimelineItem>
-                            <TimelineOppositeContent>
-                                <Typography variant="body2" color="text.primar">2013</Typography>
-                            </TimelineOppositeContent>
-                            <TimelineSeparator>
-                                <TimelineDot variant="outlined" />
-                            </TimelineSeparator>
-                            <TimelineContent>
-                                <Paper elevation={3} className="education_background">
-                                    <Typography variant="h6" component="h1">10<sup>th</sup> Stands</Typography>
-                                    <Typography>Naktala High School</Typography>
-                                </Paper>
-                            </TimelineContent>
-                        </TimelineItem>
-                    </Timeline>
-                </Grid>
+                {
+                    loading ?
+                        <Grid xs={12}>
+                            <Timeline align="alternate">
+                                <TimelineItem>
+                                    <TimelineOppositeContent>
+                                        <Typography variant="body2" color="text.primar"></Typography>
+                                    </TimelineOppositeContent>
+                                    <TimelineSeparator>
+                                        <TimelineDot variant="outlined" color="primary" />
+                                        <TimelineConnector className="lineHeight" />
+                                    </TimelineSeparator>
+                                    <TimelineContent>
+                                        <Paper elevation={3} className="education_background">
+                                            <Skeleton animation="wave" />
+                                            <Skeleton animation="wave" />
+                                            <Typography>Loading...</Typography>
+                                        </Paper>
+                                    </TimelineContent>
+                                </TimelineItem>
+                                <TimelineItem>
+                                    <TimelineOppositeContent>
+                                        <Typography variant="body2" color="secondary"></Typography>
+                                    </TimelineOppositeContent>
+                                    <TimelineSeparator>
+                                        <TimelineDot  variant="outlined" color="primary" />
+                                        {/* <TimelineConnector className="lineHeight" /> */}
+                                    </TimelineSeparator>
+                                    <TimelineContent>
+                                        <Paper elevation={3} className="education_background">
+                                            <Skeleton animation="wave" />
+                                            <Skeleton animation="wave" />
+                                            <Typography>Loading...</Typography>
+                                        </Paper>
+                                    </TimelineContent>
+                                </TimelineItem>
+                            </Timeline>
+                        </Grid>
+                    :
+                        <Grid xs={12}>
+                            <Timeline align="alternate">
+                                {
+                                    education.data.map((data, key) => (
+                                        <TimelineItem>
+                                            <TimelineOppositeContent>
+                                                {key%2 != 0 ? <Typography variant="body2" color="secondary">{data.start_year} - {data.is_continue ? 'Continue' : data.end_year}</Typography> : <Typography variant="body2" color="primary">{data.start_year} - {data.is_continue ? 'Continue' : data.end_year}</Typography>}
+                                            </TimelineOppositeContent>
+                                            <TimelineSeparator>
+                                                {key%2 !== 0 ? <TimelineDot variant="outlined" color="secondary" /> : <TimelineDot variant="outlined" color="primary" />}
+                                                { education.data.length-1 !== key ? <TimelineConnector className="lineHeight" /> : null}
+                                            </TimelineSeparator>
+                                            <TimelineContent>
+                                                <Paper elevation={3} className="education_background">
+                                                    <Typography variant="h6" component="h1">{data.streem}</Typography>
+                                                    <Typography>{data.institute_name}</Typography>
+                                                </Paper>
+                                            </TimelineContent>
+                                        </TimelineItem>
+                                    ))
+                                }
+                            </Timeline>
+                        </Grid>
+
+                }
             </Grid>
             <Grid container spacing={2}>
                 <Grid xs={6}>
