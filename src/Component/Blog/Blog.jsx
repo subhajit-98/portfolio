7b6e6834faff78ed8e3affe99b52c139383671ac
subhajit-98 <subhajitdey.1998@outlook.com>
2021-07-16 16:45:35 +0530

    Blog page messsage

--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -1,5 +1,10 @@
 const Blog = () => (
-    <div>Coming Soon...</div>
+    <div style={{width: "100%",
+        height: '83vh',
+        display: 'flex',
+        alignItems: 'center',
+        textAlign: 'center',
+        justifyContent: 'center'}}>Delicious Contains Prepare For You. Comming soon...</div>
 )
 
 export default Blog;
